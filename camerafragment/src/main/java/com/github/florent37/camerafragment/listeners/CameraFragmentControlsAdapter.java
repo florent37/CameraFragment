c94265669b917a75a42f95354af3124368786a0b
Skalaw <mskalski6@wp.pl>
2017-02-25 09:46:21 +0100

    Derive from Adapter and only override what you need

---
+++ camerafragment/src/main/java/com/github/florent37/camerafragment/listeners/CameraFragmentControlsAdapter.java
@@ -0,0 +1,33 @@
+package com.github.florent37.camerafragment.listeners;
+
+/**
+ * Convenience implementation of {@link CameraFragmentControlsListener}. Derive from this and only override what you need.
+ * @author Skala
+ */
+
+public class CameraFragmentControlsAdapter implements CameraFragmentControlsListener {
+    @Override
+    public void lockControls() {
+
+    }
+
+    @Override
+    public void unLockControls() {
+
+    }
+
+    @Override
+    public void allowCameraSwitching(boolean allow) {
+
+    }
+
+    @Override
+    public void allowRecord(boolean allow) {
+
+    }
+
+    @Override
+    public void setMediaActionSwitchVisible(boolean visible) {
+
+    }
+}
---
+++ camerafragment/src/main/java/com/github/florent37/camerafragment/listeners/CameraFragmentVideoRecordTextAdapter.java
@@ -0,0 +1,28 @@
+package com.github.florent37.camerafragment.listeners;
+
+/**
+ * Convenience implementation of {@link CameraFragmentVideoRecordTextListener}. Derive from this and only override what you need.
+ * @author Skala
+ */
+
+public class CameraFragmentVideoRecordTextAdapter implements CameraFragmentVideoRecordTextListener {
+    @Override
+    public void setRecordSizeText(long size, String text) {
+
+    }
+
+    @Override
+    public void setRecordSizeTextVisible(boolean visible) {
+
+    }
+
+    @Override
+    public void setRecordDurationText(String text) {
+
+    }
+
+    @Override
+    public void setRecordDurationTextVisible(boolean visible) {
+
+    }
+}
